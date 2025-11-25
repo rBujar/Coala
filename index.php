7d970199145fb1e6b195801fc026767d53643891
Bujar Roka <rokabujar@gmail.com>
2025-11-25 12:57:52 +0100

    created and styled product section

--- index.php
+++ index.php
@@ -56,6 +56,33 @@
       </div>
     </section>
   </div>
+  <section class="product container">
+    <div class="row">
+      <div class="product__title col-12">
+        <h1>Multi Useful Components</h1>
+        <p class="text-xl text-medium">We have created a new product that will help designers, developers and companies create websites for their startups quickly and easily.</p>
+      </div>
+      <div class="product__item col-12">
+        <div class="product__components">
+          <div class="product__component">
+            <h4>Retina Ready</h4>
+            <p class="text-sm text-regular">Startup Framework works fine on devices supporting Retina Desplay. Feel the clarity!</p>
+          </div>
+          <div class="product__component">
+            <h4>Responsive Layout</h4>
+            <p class="text-sm text-regular">Of course we haven't forgotten about the responsive layout. Full mobile support.</p>
+          </div>
+          <div class="product__component">
+            <h4>Full Contol</h4>
+            <p class="text-sm text-regular">Easily change and tweak your content when you need to, however you want. No more third party vendor-lock-in.</p>
+          </div>
+        </div>
+        <div class="product__phone">
+          <img src="/src//assets/phone.svg" alt="phone image">
+        </div>
+      </div>
+    </div>
+  </section>
   <script type="module" src="http://localhost:5173/src/js/main.js"></script>
 </body>
 </html>
