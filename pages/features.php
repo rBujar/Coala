97192db565220fb7b25d771961f1874530ab884e
Bujar Roka <rokabujar@gmail.com>
2025-11-28 19:17:06 +0100

    created and designed info section for the feature page

--- pages/features.php
+++ pages/features.php
@@ -12,7 +12,7 @@
 <body>
     <main>
         <?php include '../components/header.php'?>
-        <section class="feature-hero container">
+    <section class="feature-hero container">
             <div class="row">
                 <div class="feature-hero__title col-12 col-xl-8 offset-xl-2">
                     <h1>Create Something Awesome</h1>
@@ -40,6 +40,38 @@
                 </div>
             </div>
         </section>
+        <div class="default-bg overlay">
+            <section class="info container">
+                <div class="row">
+                    <div class="info__title col-12">
+                        <span class="text-xs text-bold" >Easy-to-use</span>
+                        <h1>Better Landing for Your Startup</h1>
+                        <p class="text-xl text-regular">We have created a new product that will help designers, developers and companies create websites for their startups quickly and easily.</p>
+                    </div>
+                    <div class="info__content col-12">
+                        <div class="info__cards">
+                            <div class="info__card">
+                                <i class="icon-book"></i>
+                                <div class="info__card-title">
+                                    <h4>User Guide</h4>
+                                    <p class="text-sm text-regular">You'll get a description of each component and learn how to connect and use them.</p>
+                                </div>
+                            </div>
+                            <div class="info__card">
+                                <i class="icon-youtube"></i>
+                                <div class="info__card-title">
+                                    <h4>Video Tutorials</h4>
+                                    <p class="text-sm text-regular">Video tutorials show you how to make a web site for your startup from design to code.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="info__image">
+                            <img src="/src/assets/macbook.svg" alt="macbook image">
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
         <section class="benefits container">
             <div class="row">
                 <div class="benefits__content col-12">
