bccf4b2a6ef40a9794ba36a5f54fb4bccb9b2855
Bujar Roka <rokabujar@gmail.com>
2025-12-16 17:28:58 +0100

    reverted to php

--- index.php
+++ index.php
@@ -3,37 +3,12 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="/src/css/style.scss" />
+  <link rel="stylesheet" href="http://localhost:5173/src/css/style.scss" />
   <link rel="shortcut icon" type="image/x-icon" href="/src/assets/favicon.ico" />
   <title>Coala</title>
 </head>
 <body>
-<header class="header container">
-    <div class="header__logo">
-        <h2>Coala</h2>
-    </div>
-    <nav class="header__nav">
-        <div class="header__buttons container">
-            <button class="header__buttons--signIn">
-                <p class="text-md text-regular">Sign in</p>
-            </button>
-            <button class="green-btn header__buttons--signUp">
-                <p class="text-md text-regular">Sing Up</p>
-            </button>
-        </div>
-        <ul class="header__menu container">
-            <li class="header__item"><a href="../index.php">Home</a></li>
-            <li class="header__item"><a href="../pages/features.php">Features</a></li>
-            <li class="header__item"><a href="#">Contact Us</a></li>
-            <li class="header__item"><a href="../pages/pricing.php">Pricing</a></li>
-        </ul>
-    </nav>
-    <div class="header__hamburger">
-        <span class="header__hamburger--bar header__hamburger--bar--top"></span>
-        <span class="header__hamburger--bar header__hamburger--bar--middle"></span>
-        <span class="header__hamburger--bar header__hamburger--bar--bottom"></span>
-    </div>
-</header>
+  <?php include 'components/header.php'?>
   <section class="hero container">
     <div class="row">
       <div class="hero__item col-12 col-xl-8 offset-xl-2">
@@ -156,25 +131,7 @@
       </div>
     </div>
   </section>
-  <div class="default-bg">
-    <footer class="footer container">
-    <div class="footer__logo">
-        <h2>Coala</h2>
-    </div>
-    <div class="footer__menu">
-        <a href="#" class="text-xs text-bold">Tools</a>
-        <a href="#" class="text-xs text-bold">Faq</a>
-        <a href="#" class="text-xs text-bold">Privacy</a>
-        <a href="#" class="text-xs text-bold">Terms & Conditions</a>
-        <a href="#" class="text-xs text-bold">Contact</a>
-    </div>
-    <div class="footer__socials">
-        <a href="#"><i class="icon-twitter"></i></a>
-        <a href="#"><i class="icon-facebook"></i></a>
-        <a href="#"><i class="icon-google"></i></a>
-    </div>
-    </footer>
-</div>
-  <script type="module" src="/src/js/main.js"></script>
+  <?php include 'components/footer.php'?>
+  <script type="module" src="http://localhost:5173/src/js/main.js"></script>
 </body>
 </html>
