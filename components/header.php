9d88fddc9f16c8797dc1a600f0b0616ec5a8a6fa
Bujar Roka <rokabujar@gmail.com>
2025-12-17 17:06:12 +0100

    restyled the header

--- components/header.php
+++ components/header.php
@@ -1,9 +1,16 @@
 <header class="header container">
     <div class="header__logo">
-        <h2>Coala</h2>
+       <a href="../index.php"><h2>Coala</h2></a>
     </div>
     <nav class="header__nav">
-        <div class="header__buttons container">
+        <ul class="header__menu container">
+            <li class="header__item"><a href="../pages/features.php">Features</a></li>
+            <li class="header__item"><a href="../pages/contactUs.php">Contact Us</a></li>
+            <li class="header__item"><a href="../pages/pricing.php">Pricing</a></li>
+        </ul>
+    </nav>
+    <div class="header__content">
+    <div class="header__buttons">
             <button class="header__buttons--signIn">
                 <p class="text-md text-regular">Sign in</p>
             </button>
@@ -22,5 +29,6 @@
         <span class="header__hamburger--bar header__hamburger--bar--top"></span>
         <span class="header__hamburger--bar header__hamburger--bar--middle"></span>
         <span class="header__hamburger--bar header__hamburger--bar--bottom"></span>
+    </div>
     </div>
 </header>
