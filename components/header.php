6ac5f4c8db2ed52349b8a088f0df0af36952e1d3
Bujar Roka <rokabujar@gmail.com>
2025-12-30 11:03:56 +0100

    minor change

--- components/header.php
+++ components/header.php
@@ -18,13 +18,6 @@
                 <p class="text-sm text-regular">Sing Up</p>
             </button>
         </div>
-        <ul class="header__menu container">
-            <li class="header__item"><a href="../index.php">Home</a></li>
-            <li class="header__item"><a href="../pages/features.php">Features</a></li>
-            <li class="header__item"><a href="../pages/contactUs.php">Contact Us</a></li>
-            <li class="header__item"><a href="../pages/pricing.php">Pricing</a></li>
-        </ul>
-    </nav>
     <div class="header__hamburger">
         <span class="header__hamburger--bar header__hamburger--bar--top"></span>
         <span class="header__hamburger--bar header__hamburger--bar--middle"></span>
