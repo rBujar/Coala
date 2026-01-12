0bbca4f93030ef28dbec293fb5c815b4c6a057e2
Bujar Roka <rokabujar@gmail.com>
2026-01-12 17:19:14 +0100

    added and styled values section

--- pages/aboutUs.php
+++ pages/aboutUs.php
@@ -16,7 +16,7 @@
         <section class="team container">
             <div class="row">
                 <div class="team__title col-12">
-                    <h2>Our team</h2>
+                    <h2 class="h2-style">Our team</h2>
                     <p class="text-lg text-regular">Worem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.</p>
                 </div>
                 <div class="team__cards col-12">
@@ -75,6 +75,34 @@
                                 <i class="icon-github"></i>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="values container">
+            <div class="row">
+                <h2 class="h2-style col-12">Our values</h2>
+                <div class="values__cards col-12">
+                    <div class="values__item">
+                        <div class="values__title">
+                            <h4>Design to Impress</h4>
+                            <i class="icon-send"></i>
+                        </div>
+                        <p class="text-sm text-regular">Carefully crafted precise design, with harmonious typography and perfect padding around every component.</p>
+                    </div>
+                    <div class="values__item">
+                        <div class="values__title">
+                            <h4>Commited Support</h4>
+                            <i class="icon-support"></i>
+                        </div>
+                        <p class="text-sm text-regular">We are here for you. Our dedicated team is committed in providing excellent support.</p>
+                    </div>
+                    <div class="values__item">
+                        <div class="values__title">
+                            <h4>Clean Code</h4>
+                            <i class="icon-code_file"></i>
+                        </div>
+                        <p class="text-sm text-regular">Built with HTML, CSS and JS. Startup's code is easy to under-stand and easy to change. No other frameworks needed.</p>
                     </div>
                 </div>
             </div>
